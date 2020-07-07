236bfc8407e54a40e1fe361f75f5b81500953ca0
Kai Vandivier <kpvandivier@gmail.com>
2020-07-07 13:54:26 -0700

    Add category list and detail view

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -8,36 +8,65 @@ const Recipe = require("../models/recipe");
 
 // Get form for category creation
 exports.categoryCreateGet = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: categoryCreateGet")
-}
+  res.send("NOT IMPLEMENTED: categoryCreateGet");
+};
 // POST endpoint to create new category
 exports.categoryCreatePost = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: categoryCreatePost")
-}
+  res.send("NOT IMPLEMENTED: categoryCreatePost");
+};
+
+
 
 // GET a list of all categories
-exports.categoriesListGet = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: categoriesListGet")
-}
+exports.categoryListGet = function (req, res, next) {
+  Category.find((err, categories) => {
+    if (err) return next(err);
+    res.render("categoryList", {
+      title: "Categories",
+      categories,
+    })
+  })
+};
+
 // GET details of one category
 exports.categoryDetailGet = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: categoryDetailGet")
-}
+  async.parallel(
+    {
+      category: (cb) =>
+        Category.findById(req.params.id)
+          .orFail("Can't find that category")
+          .exec(cb),
+      categoryItems: (cb) => Item.find({ category: req.params.id }, cb),
+    },
+    (err, { category, categoryItems }) => {
+      if (err) return next(err);
+      res.render("categoryDetail", {
+        title: "Category",
+        category,
+        categoryItems,
+      });
+    }
+  );
+};
+
+
 
 // GET form to update category
 exports.categoryUpdateGet = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: categoryUpdateGet")
-}
+  res.send("NOT IMPLEMENTED: categoryUpdateGet");
+};
 // POST endpoint to update category
 exports.categoryUpdatePost = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: categoryUpdatePost")
-}
+  res.send("NOT IMPLEMENTED: categoryUpdatePost");
+};
+
+
 
 // GET form to delete category
 exports.categoryDeleteGet = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: categoryDeleteGet")
-}
+  res.send("NOT IMPLEMENTED: categoryDeleteGet");
+};
 // POST endpoint to delete category
 exports.categoryDeletePost = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: categoryDeletePost")
-}
+  res.send("NOT IMPLEMENTED: categoryDeletePost");
+};
